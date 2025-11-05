991ef83988e63a97239a300ba62e2b339f634d98
inexglobal <chaiwat@inex.co.th>
2025-11-05 10:57:49 +0700

    Update EX13_Serial.ino

--- examples/EX13_Serial/EX13_Serial.ino
+++ examples/EX13_Serial/EX13_Serial.ino
@@ -2,13 +2,22 @@
 #define TX_PIN 5
 #define mySerial Serial1
 void setup() {
+  Serial.begin(115200);
+
   pinMode(RX_PIN, INPUT_PULLUP);
   mySerial.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);
 }
 void loop() {
-  if (mySerial.available()) {
-    char strMsg = mySerial.read();
-    mySerial.write(strMsg);
+
+  if (Serial.available()) {
+    char c = Serial.read();
+    mySerial.write(c);
     mySerial.flush();
+  }
+
+  if (mySerial.available()) {
+    char c = mySerial.read();
+    Serial.write(c);
+    Serial.flush();
   }
 }
