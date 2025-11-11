c2e14f8ecc2742c37c47f7229a00995dfae6107e
inexglobal <chaiwat@inex.co.th>
2025-11-11 11:44:33 +0700

    update

---
+++ iCON32-ExampleCode/iCON32-DHT11_OLED/iCON32-DHT11_OLED.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+#include "DHT.h"
+#define DHTPIN 6
+#define DHTTYPE DHT11 
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  dht.begin();
+  Wire.begin(2,3,400000);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.setTextColor(SSD1306_WHITE,SSD1306_BLACK);
+  display.setTextSize(2); // Draw 2X-scale text
+  display.clearDisplay();
+  display.display();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    return;
+  }
+  display.setCursor(0,0);
+  display.println("Temp:"+String(t)+"  ");
+  display.println("Humi:"+String(h)+"  ");
+  display.display();
+}
+
+
--- iCON32-ExampleCode/iCON32-ServoAngleTest/iCON32-ServoAngleTest.ino
+++ iCON32-ExampleCode/iCON32-ServoAngleTest/iCON32-ServoAngleTest.ino
@@ -5,9 +5,9 @@
 
 #define MAX_RESOLUTION 16     // 16 Bbit = 0 - 65535
 #define SERVO_FREQUENCY 50    // Servo motor's pulse frequency 50Hz
-#define SERVO_MAX_ANGLE 270   // Max angle
+#define SERVO_MAX_ANGLE 180   // Max angle
 #define SERVO_MIN_TICKS 1700  // Angle 0 degree
-#define SERVO_MAX_TICKS 8000  // Angle 270 degree
+#define SERVO_MAX_TICKS 8000  // Angle SERVO_MAX_ANGLE degree
 
 void setup() {
   // Set pin, frequency and resolution
@@ -23,5 +23,5 @@ void loop() {
   Servo(PIN_SERVO_7, 0);    // Drive servo motor pin 7 to angle 0
   Servo(PIN_SERVO_8, 90);   // Drive servo motor pin 8 to angle 90
   Servo(PIN_SERVO_9, 135);  // Drive servo motor pin 9 to angle 135
-  Servo(PIN_SERVO_10, 270); // Drive servo motor pin 10 to angle 270
+  Servo(PIN_SERVO_10, 180); // Drive servo motor pin 10 to angle 180
 }
